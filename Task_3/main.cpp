8835daed6f96629594fdcedde4d89760bf8782f3
maksim <WalkLU@yandex.ru>
2024-11-11 18:11:35 +0300

    PW-30-V1.01

--- Task_3/main.cpp
+++ Task_3/main.cpp
@@ -7,15 +7,17 @@ int main() {
     std::string choice;
     std::map<std::string , std::string> args;
     while(choice != "get" && choice != "post") {        
-        std::cout<<"\nEnter choice : ";
+        std::cout<<"\nEnter \"get\" \"post\" for request. Or any word to add key : ";
         std::cin>>choice;
-        std::string tempArg;
-        std::cout<<"\nEnter argument for key \"" << choice <<"\" : ";
-        std::cin>>tempArg;
-        args[choice] = tempArg;        
+        if(choice != "get" && choice != "post") {
+            std::string tempArg;
+            std::cout<<"\nEnter argument for key \"" << choice <<"\" : ";
+            std::cin>>tempArg;
+            args[choice] = tempArg;    
+        }    
     }
     std::map<std::string,std::string>::iterator iter = args.begin();
-    if(iter != args.end()){
+    if(iter != args.end()) {
         if(choice == "post") {             
             std::vector<cpr::Pair> pairVec;
             for(;iter != args.end();++iter){
@@ -35,7 +37,7 @@ int main() {
             std::cout<<r.text<<std::endl;
         }    
     }
-    else{
+    else {
         std::cout<<"\nNothing to post or get"<<std::endl;
     }
 }
