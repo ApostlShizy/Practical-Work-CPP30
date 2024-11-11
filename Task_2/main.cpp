95a4f7bf4e06a72471ccee3a6a35f11e29f01f36
maksim <WalkLU@yandex.ru>
2024-11-11 18:30:51 +0300

    PW-30-V1.01

--- Task_2/main.cpp
+++ Task_2/main.cpp
@@ -3,8 +3,8 @@
 
 int main() {
     cpr::Response r = cpr::Get(cpr::Url("http://httpbin.org//html"));    
-    std::string startStr = "h1";
-    std::string endStr = "/h1";
+    std::string startStr = "<h1>";
+    std::string endStr = "</h1>";
 
     int startStrLen = startStr.length();
     int start = r.text.find(startStr) + startStrLen;
